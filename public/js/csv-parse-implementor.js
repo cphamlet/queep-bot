b9855d846edd445fe628841c75e4e05b50a68677
Connor Hamlet <cphamlet@live.unc.edu>
2018-12-10 08:52:41 -0600

    Updated CSV Parse Updates

--- public/js/csv-parse-implementor.js
+++ public/js/csv-parse-implementor.js
@@ -1,38 +1,37 @@
-$(function() {
+'use strict';
+
+$(function () {
     // Handler for .ready() called.
-    $('#fileUpload').on("change", function(){ 
-        $('#fileUpload[type=file]').parse(
-            {
-                config: {
-                    //When a file has completed parsing, it will run the complete function. 
-                    complete:function(results, file){
-                        //Converts 2d Array to json.
-                        csvToJSON(results.data);
-                    }
-                },
-                //Executes before the file is parsed
-                before:function(file, inputElem){
-                    if(!file || !file.name.endsWith(".csv")){
-                        return {action:"abort", reason:"Invalid file, use a CSV file."};
-                    }
-                },
-                error: function(err, file, inputElem, reason)
-                {
-                    alert(reason);
-                    // executed if an error occurs while loading the file,
-                    // or if before callback aborted for some reason
+    $('#fileUpload').on("change", function () {
+        $('#fileUpload[type=file]').parse({
+            config: {
+                //When a file has completed parsing, it will run the complete function. 
+                complete: function complete(results, file) {
+                    //Converts 2d Array to json.
+                    csvToJSON(results.data);
                 }
+            },
+            //Executes before the file is parsed
+            before: function before(file, inputElem) {
+                if (!file || !file.name.endsWith(".csv")) {
+                    return { action: "abort", reason: "Invalid file, use a CSV file." };
+                }
+            },
+            error: function error(err, file, inputElem, reason) {
+                alert(reason);
+                // executed if an error occurs while loading the file,
+                // or if before callback aborted for some reason
             }
-        ); //end Papa.parse();
+        }); //end Papa.parse();
     }); //end on function
 });
 
-function csvToJSON(csv_data){
+function csvToJSON(csv_data) {
     console.log("Load complete");
     word_acro_data = {};
-    for(var i = 1; i<csv_data.length-1; i++){
+    for (var i = 1; i < csv_data.length - 1; i++) {
         var acronym = csv_data[i][0];
-        acronym = $("<div />").text(acronym).html()
+        acronym = $("<div />").text(acronym).html();
         //TODO, if the spelled out word has a special html char, it will not be 
         //imported properly, need to escape
         var spelledOutWords = csv_data[i][1].split(",");
@@ -42,7 +41,7 @@ function csvToJSON(csv_data){
     triggerAnimation();
 }
 
-function triggerAnimation(){
+function triggerAnimation() {
     M.toast({html: 'CSV Uploaded Sucessfully! &#x2713;'});
     return;
 }
